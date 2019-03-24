5ce07b5ce0f35e121e56f2522a25d5443f24f44f
John-John Tedro <udoprog@tedro.se>
2019-03-24 18:32:57 +0100

    Improve setmod.ps1 script

--- tools/setmod.ps1
+++ tools/setmod.ps1
@@ -1,10 +1,17 @@
 # This is a helper script to run setmod in a loop, restarting if it shuts down.
 # It can be evoked from anywhere on the filesystem.
+param([switch] $Release=$false)
 
 . "$PSScriptRoot\env.ps1"
 
+$Args = @()
+
+if ($Release) {
+    $Args += "--release"
+}
+
 while($true) {
-    cargo run --manifest-path="$PSScriptRoot\..\bot\Cargo.toml"
+    cargo run --manifest-path="$PSScriptRoot\..\bot\Cargo.toml" "$Args"
     Write-Host "Bot shut down, restarting in 5s..."
     Start-Sleep -s 5
 }
