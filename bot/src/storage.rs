708d322363b4ee534280e82d937998f030df1364
John-John Tedro <udoprog@tedro.se>
2019-07-27 14:22:17 +0200

    Bump futures-cache dependency

--- bot/src/storage.rs
+++ bot/src/storage.rs
@@ -1,35 +1,21 @@
 use failure::Error;
 use std::{path::Path, sync::Arc};
 
-pub use futures_cache::Cache;
+pub use futures_cache::{sled, Cache};
 
 pub struct Storage {
-    cache: Arc<rocksdb::DB>,
+    db: Arc<sled::Db>,
 }
 
 impl Storage {
     /// Open the given storage location.
     pub fn open(path: &Path) -> Result<Storage, Error> {
-        let cache = create_db(&path.join("cache"))?;
-        Ok(Storage { cache })
+        let db = sled::Db::start_default(path.join("sled.24"))?;
+        Ok(Storage { db: Arc::new(db) })
     }
 
     /// Access the cache abstraction of your storage.
     pub fn cache(&self) -> Result<Cache, Error> {
-        Ok(Cache::load(self.cache.clone())?)
+        Ok(Cache::load(self.db.open_tree("cache")?)?)
     }
-}
-
-/// Create a new environment based on the given path.
-fn create_db(path: &Path) -> Result<Arc<rocksdb::DB>, Error> {
-    if !path.is_dir() {
-        std::fs::create_dir_all(path)?;
-    }
-
-    let mut options = rocksdb::Options::default();
-    options.set_compression_type(rocksdb::DBCompressionType::Snappy);
-    options.set_keep_log_file_num(16);
-    options.create_if_missing(true);
-
-    Ok(Arc::new(rocksdb::DB::open(&options, path)?))
 }
