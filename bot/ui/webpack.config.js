ac04281db12afe1001e90858e9ecce3db7c195bc
John-John Tedro <udoprog@tedro.se>
2019-07-27 19:04:37 +0200

    Clean up some web assets and bump dependencies

--- bot/ui/webpack.config.js
+++ bot/ui/webpack.config.js
@@ -8,48 +8,58 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 const faviconPlugin = new FaviconsWebpackPlugin("../res/icon.png");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-            "style-loader", // creates style nodes from JS strings
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
-    ]
-  },
-  plugins: [htmlPlugin, faviconPlugin],
-  devServer: {
-    historyApiFallback: true,
-    proxy: {
-      '/ws/*': {
-        target: 'ws://localhost:12345',
-        secure: false,
-        ws: true,
-      },
-      '/api': {
-        target: 'http://localhost:12345',
-        secure: false,
+module.exports = function(_, argv) {
+  let production = argv.mode === "production";
+
+  return {
+    optimization: {
+      minimize: production
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {},
+            },
+          ],
+        },
+      ]
+    },
+    plugins: [
+      htmlPlugin,
+      faviconPlugin,
+    ],
+    devServer: {
+      historyApiFallback: true,
+      proxy: {
+        '/ws/*': {
+          target: 'ws://localhost:12345',
+          secure: false,
+          ws: true,
+        },
+        '/api': {
+          target: 'http://localhost:12345',
+          secure: false,
+        },
       },
     },
-  },
+  };
 };
