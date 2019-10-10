cb34ff4d2369ed325952c8a02b2553ca076e7fae
John-John Tedro <udoprog@tedro.se>
2019-10-10 11:01:45 +0200

    Build using Rust tool on Travis as well

--- tools/builder/src/main.rs
+++ tools/builder/src/main.rs
@@ -1,38 +1,134 @@
-use anyhow::{anyhow, bail, Error};
+use anyhow::{anyhow, bail, Result};
 use regex::Regex;
 use std::{
     env,
     ffi::OsStr,
-    fs,
+    fmt, fs, io,
     path::{Path, PathBuf},
     process::Command,
 };
 use walkdir::WalkDir;
 
-type Result<T, E = Error> = std::result::Result<T, E>;
+#[cfg(target_os = "windows")]
+pub struct SignTool {
+    root: PathBuf,
+    signtool: PathBuf,
+    password: String,
+}
+
+#[cfg(target_os = "windows")]
+impl SignTool {
+    /// Construct a new signtool signer.
+    pub fn open(root: PathBuf, signtool: PathBuf, password: String) -> Option<Self> {
+        if !signtool.is_file() {
+            return None;
+        }
+
+        Some(Self {
+            root,
+            signtool,
+            password,
+        })
+    }
+
+    /// Sign the given path with the given description.
+    fn sign(&self, path: &Path, what: &str) -> Result<()> {
+        println!("Signing: {}", path.display());
+
+        let cert = self.root.join("bot/res/cert.pfx");
+
+        let status = Command::new(&self.signtool)
+            .args(&["sign", "/f"])
+            .arg(&cert)
+            .args(&[
+                "/d",
+                what,
+                "/du",
+                "https://github.com/udoprog/OxidizeBot",
+                "/p",
+                self.password.as_str(),
+            ])
+            .arg(path)
+            .status()?;
+
+        if !status.success() {
+            bail!("failed to run signtool");
+        }
+
+        Ok(())
+    }
+}
 
 /// Calculate an MSI-safe version number.
 /// Unfortunately this enforces some unfortunate constraints on the available
 /// version range.
 ///
 /// The computed patch component must fit within 65535
-fn msi_version(major: u32, minor: u32, patch: u32, pre: Option<u32>) -> Result<String> {
-    if patch > 64 {
-        bail!("patch version must not be greater than 64: {}", patch);
+#[cfg(target_os = "windows")]
+fn msi_version(version: &Version) -> Result<String> {
+    if version.patch > 64 {
+        bail!(
+            "patch version must not be greater than 64: {}",
+            version.patch
+        );
     }
 
     let mut last = 999;
 
-    if let Some(pre) = pre {
+    if let Some(pre) = version.pre {
         if pre >= 999 {
-            bail!("patch version must not be greater than 64: {}", patch);
+            bail!(
+                "patch version must not be greater than 64: {}",
+                version.patch
+            );
         }
 
         last = pre;
     }
 
-    last += patch * 1000;
-    Ok(format!("{}.{}.{}", major, minor, last))
+    last += version.patch * 1000;
+    Ok(format!("{}.{}.{}", version.major, version.minor, last))
+}
+
+#[derive(Debug, Clone)]
+struct Version {
+    base: String,
+    major: u32,
+    minor: u32,
+    patch: u32,
+    pre: Option<u32>,
+}
+
+impl Version {
+    /// Open a version by matching it against the given string.
+    pub fn open(version: impl AsRef<str>) -> Result<Option<Version>> {
+        let version_re = Regex::new(r"^(\d+)\.(\d+)\.(\d+)(-.+\.(\d+))?$")?;
+        let version = version.as_ref();
+
+        let m = match version_re.captures(version) {
+            Some(m) => m,
+            None => return Ok(None),
+        };
+
+        let major: u32 = str::parse(&m[1])?;
+        let minor: u32 = str::parse(&m[2])?;
+        let patch: u32 = str::parse(&m[3])?;
+        let pre: Option<u32> = m.get(5).map(|s| str::parse(s.as_str())).transpose()?;
+
+        Ok(Some(Self {
+            base: version.to_string(),
+            major,
+            minor,
+            patch,
+            pre,
+        }))
+    }
+}
+
+impl fmt::Display for Version {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.base.fmt(fmt)
+    }
 }
 
 fn cargo(args: &[&str]) -> Result<()> {
@@ -46,67 +142,90 @@ fn cargo(args: &[&str]) -> Result<()> {
     Ok(())
 }
 
-fn archive(path: &Path, file: impl AsRef<Path>) -> Result<()> {
-    let status = Command::new("7z")
-        .arg("a")
-        .arg(path)
-        .arg(file.as_ref())
-        .status()?;
+fn create_zip(file: &Path, it: impl IntoIterator<Item = PathBuf>) -> Result<()> {
+    let options =
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
 
-    if !status.success() {
-        bail!("failed to run 7z");
+    let mut zip = zip::ZipWriter::new(fs::File::create(file)?);
+    let mut it = it.into_iter();
+
+    while let Some(p) = it.next() {
+        let file_name = p
+            .file_name()
+            .ok_or_else(|| anyhow!("file without file name"))?
+            .to_str()
+            .ok_or_else(|| anyhow!("file name is not a string"))?;
+        zip.start_file(file_name, options)?;
+        let mut from = fs::File::open(&p)?;
+        io::copy(&mut from, &mut zip)?;
+    }
+
+    zip.finish()?;
+    Ok(())
+}
+
+/// Copy a bunch of files with the matching file extension from one directory to another.
+fn copy_files<F>(from: &Path, target: &Path, ext: &str, visitor: F) -> Result<()>
+where
+    F: Fn(&Path) -> Result<()>,
+{
+    let mut files = Vec::new();
+
+    for e in WalkDir::new(from) {
+        let e = e?;
+
+        if e.path().extension() == Some(OsStr::new(ext)) {
+            files.push(e.path().to_owned());
+        }
+    }
+
+    for installer in &files {
+        let name = installer
+            .file_name()
+            .ok_or_else(|| anyhow!("no file name"))?;
+
+        let target = target.join(name);
+
+        fs::copy(installer, &target)?;
+        visitor(&target)?;
     }
 
     Ok(())
 }
 
-fn sign(
-    signtool: &Path,
-    root: &Path,
-    certificate_password: &str,
-    path: &Path,
-    what: &str,
-) -> Result<()> {
-    println!("Signing: {}", path.display());
-
-    let cert = root.join("bot/res/cert.pfx");
-
-    let status = Command::new(signtool)
-        .args(&["sign", "/f"])
-        .arg(&cert)
-        .args(&[
-            "/d",
-            what,
-            "/du",
-            "https://github.com/udoprog/OxidizeBot",
-            "/p",
-            certificate_password,
-        ])
-        .arg(path)
-        .status()?;
-
-    if !status.success() {
-        bail!("failed to run signtool");
-    }
+/// Create a zip distribution.
+fn create_zip_dist(root: &Path, exe: PathBuf, version: &Version) -> Result<()> {
+    create_zip(
+        &root.join(format!(
+            "oxidize-{version}-{os}-{arch}.zip",
+            version = version,
+            os = std::env::consts::OS,
+            arch = std::env::consts::ARCH
+        )),
+        vec![root.join("README.md"), exe],
+    )?;
 
     Ok(())
 }
 
-fn main() -> Result<()> {
-    let root = Path::new(env!("CARGO_MANIFEST_DIR")).join("..\\..");
+/// Perform a Windows build.
+#[cfg(target_os = "windows")]
+fn windows_build(root: &Path) -> Result<()> {
+    let version = match env::var("APPVEYOR_REPO_TAG_NAME").ok() {
+        Some(version) => Version::open(version)?,
+        None => None,
+    };
 
-    println!("ROOT: {}", root.display());
+    let signer = match (env::var("SIGNTOOL"), env::var("CERTIFICATE_PASSWORD")) {
+        (Ok(signtool), Ok(password)) => {
+            SignTool::open(root.to_owned(), PathBuf::from(signtool), password)
+        }
+        _ => None,
+    };
 
-    let version = env::var("APPVEYOR_REPO_TAG_NAME")
-        .map_err(|_| anyhow!("missing: APPVEYOR_REPO_TAG_NAME"))?;
-    let certificate_password = env::var("CERTIFICATE_PASSWORD").ok();
-    let signtool = env::var("SIGNTOOL").map(PathBuf::from).ok();
-
-    let version_re = Regex::new(r"^(\d+)\.(\d+)\.(\d+)(-.+\.(\d+))?$")?;
-
-    // is this a release?
-    let m = match version_re.captures(&version) {
-        Some(m) => m,
+    // Is this a release?
+    let version = match version {
+        Some(version) => version,
         None => {
             println!("Testing...");
             cargo(&["build", "--all"])?;
@@ -116,30 +235,20 @@ fn main() -> Result<()> {
     };
 
     let exe = root.join("target/release/oxidize.exe");
-    let wix_dir = root.join("target/wix");
-
-    let major: u32 = str::parse(&m[1])?;
-    let minor: u32 = str::parse(&m[2])?;
-    let patch: u32 = str::parse(&m[3])?;
-    let pre: Option<u32> = m.get(5).map(|s| str::parse(s.as_str())).transpose()?;
 
     if !exe.is_file() {
         println!("building: {}", exe.display());
         cargo(&["build", "--release", "--bin", "oxidize"])?;
     }
 
-    if let (Some(signtool), Some(certificate_password)) = (&signtool, &certificate_password) {
-        sign(
-            signtool,
-            &root,
-            certificate_password,
-            &root.join("target/release/oxidize.exe"),
-            "OxidizeBot",
-        )?;
+    if let Some(signer) = signer.as_ref() {
+        signer.sign(&exe, "OxidizeBot")?;
     }
 
+    let wix_dir = root.join("target/wix");
+
     if !wix_dir.is_dir() {
-        let msi_version = msi_version(major, minor, patch, pre)?;
+        let msi_version = msi_version(&version)?;
 
         cargo(&[
             "wix",
@@ -151,37 +260,73 @@ fn main() -> Result<()> {
         ])?;
     }
 
-    let mut installers = Vec::new();
-
-    for e in WalkDir::new(&wix_dir) {
-        let e = e?;
-
-        if e.path().extension() == Some(OsStr::new("msi")) {
-            installers.push(e.path().to_owned());
+    copy_files(&wix_dir, &root, "msi", |file| {
+        if let Some(signer) = &signer {
+            signer.sign(file, "OxidizeBot Installer")?;
         }
-    }
 
-    if let (Some(tool), Some(certificate_password)) = (&signtool, &certificate_password) {
-        for installer in &installers {
-            sign(
-                tool,
-                &root,
-                certificate_password,
-                installer,
-                "OxidizeBot Installer",
-            )?;
+        Ok(())
+    })?;
+
+    create_zip_dist(&root, exe, &version)?;
+    Ok(())
+}
+
+/// Perform a Linux build.
+#[cfg(target_os = "linux")]
+fn linux_build(root: &Path) -> Result<()> {
+    let version = match env::var("TRAVIS_TAG") {
+        Ok(version) if version != "" => Version::open(&version)?,
+        _ => None,
+    };
+
+    let pull_request = match env::var("TRAVIS_PULL_REQUEST") {
+        Ok(pull_request) if pull_request != "false" => Some(str::parse::<u32>(&pull_request)?),
+        _ => None,
+    };
+
+    let version = match (&pull_request, &version) {
+        (None, Some(version)) => version,
+        _ => {
+            println!("Testing...");
+            cargo(&["build", "--all"])?;
+            cargo(&["test", "--all"])?;
+            return Ok(());
         }
+    };
+
+    let exe = root.join("target/release/oxidize");
+
+    let debian_dir = root.join("target/debian");
+
+    if !debian_dir.is_dir() {
+        cargo(&["deb", "-p", "oxidize"])?;
     }
 
-    for installer in &installers {
-        let name = installer
-            .file_name()
-            .ok_or_else(|| anyhow!("no file name"))?;
-        fs::copy(installer, root.join(name))?;
+    copy_files(&debian_dir, &root, "deb", |_| Ok(()))?;
+
+    if !exe.is_file() {
+        println!("building: {}", exe.display());
+        cargo(&["build", "--release", "--bin", "oxidize"])?;
     }
 
-    let zip = root.join(format!("oxidize-{}-windows-x86_64.zip", version));
-    archive(&zip, root.join("README.md"))?;
-    archive(&zip, root.join("target/release/oxidize.exe"))?;
+    create_zip_dist(&root, exe, &version)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../..");
+    println!("root: {}", root.display());
+
+    #[cfg(target_os = "windows")]
+    {
+        windows_build(&root)?;
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        linux_build(&root)?;
+    }
+
     Ok(())
 }
