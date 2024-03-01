956f76bb930da2948c1c07e24cc39471e1808b13
John-John Tedro <udoprog@tedro.se>
2024-03-01 11:56:21 +0100

    Use windows_subsystem by default

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(feature = "windows", windows_subsystem = "windows")]
+#![cfg_attr(all(windows, not(feature = "cli")), windows_subsystem = "windows")]
 
 pub(crate) fn main() -> anyhow::Result<()> {
     oxidize::cli::main()
